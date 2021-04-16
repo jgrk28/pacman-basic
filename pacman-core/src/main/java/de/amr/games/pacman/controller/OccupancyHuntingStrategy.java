d9c8351e7c692cf6bdbc174a94354f3e0817878c
jgrk88 <jacobkaplan88@gmail.com>
2021-04-16 12:41:10 -0400

    Fix simple bugs in occupancy implementation

--- pacman-core/src/main/java/de/amr/games/pacman/controller/OccupancyHuntingStrategy.java
+++ pacman-core/src/main/java/de/amr/games/pacman/controller/OccupancyHuntingStrategy.java
@@ -28,23 +28,26 @@ public class OccupancyHuntingStrategy extends HuntingStrategy {
   }
 
   private void initOccupancy() {
-    this.gameWorld = gameModel.currentLevel.getWorld();
-    long numTiles = gameWorld.tiles().filter(tile -> !gameWorld.isWall(tile)).count();
+    long numTiles = gameWorld.tiles().filter(
+        tile -> !gameWorld.isWall(tile) && !gameWorld.isGhostHouseDoor(tile)
+    ).count();
     Double startingProb = 1.0 / numTiles;
-    gameWorld.tiles().filter(tile -> !gameWorld.isWall(tile))
-        .forEach(tile -> occupancy.put(tile, startingProb));
+    gameWorld.tiles().filter(
+        tile -> !gameWorld.isWall(tile) && !gameWorld.isGhostHouseDoor(tile)
+    ).forEach(tile -> occupancy.put(tile, startingProb));
   }
 
   @Override
   V2i ghostHuntingTarget(int ghostID) {
     if (occupancy.size() == 0) {
+      this.gameWorld = gameModel.currentLevel.getWorld();
       initOccupancy();
     }
 
     Set<V2i> seenTiles = new HashSet<>();
     V2i pacTile = gameModel.player.tile();
     V2i ghostTile = gameModel.ghosts[ghostID].tile();
-    for (int i = 1; i <= 4; i++) {
+    for (int i = 0; i <= 4; i++) {
       V2i aheadGhost = ghostTile.plus(gameModel.ghosts[ghostID].dir.vec.scaled(i));
       seenTiles.add(aheadGhost);
     }
@@ -73,33 +76,39 @@ public class OccupancyHuntingStrategy extends HuntingStrategy {
 
   private void recalculateOccupancy(Set<V2i> seenTiles, int GhostID) {
     double wipedAmount = 0.0;
+    Set<V2i> redistributedTiles = new HashSet<>();
     for (Map.Entry<V2i, Double> tileOccupancy : occupancy.entrySet()) {
       V2i tile = tileOccupancy.getKey();
       Double occupancyValue = tileOccupancy.getValue();
       if (seenTiles.contains(tile)) {
         wipedAmount += occupancyValue;
         occupancy.replace(tile, 0.0);
+
       } else if (GhostID == BLINKY) {
         Double dispersion = occupancyValue * .01;
         modifyTileOccupancy(tile, dispersion * -1);
         Stream<V2i> validNeighbors = gameWorld.neighborTiles(tile).filter(neighbor ->
-            gameWorld.insideMap(neighbor) && !gameWorld.isWall(neighbor));
+            gameWorld.insideMap(neighbor) && !gameWorld.isWall(neighbor) && !gameWorld.isGhostHouseDoor(neighbor));
         Double amountReceived = dispersion / validNeighbors.count();
 
         //Reload stream after terminating with count
         validNeighbors = gameWorld.neighborTiles(tile).filter(neighbor ->
-            gameWorld.insideMap(neighbor) && !gameWorld.isWall(neighbor));
+            gameWorld.insideMap(neighbor) && !gameWorld.isWall(neighbor) && !gameWorld.isGhostHouseDoor(neighbor));
         validNeighbors.forEach(neighbor -> modifyTileOccupancy(neighbor, amountReceived));
       }
     }
 
     long numNonZero = occupancy.values().stream().filter(value -> !value.equals(0.0)).count();
     Double distributeWiped = wipedAmount / numNonZero;
-    for (Map.Entry<V2i, Double> tileOccupancy : occupancy.entrySet()) {
-      V2i tile = tileOccupancy.getKey();
-      Double occupancyValue = tileOccupancy.getValue();
-      if (!occupancyValue.equals(0.0)) {
-        modifyTileOccupancy(tile, distributeWiped);
+    if (numNonZero == 0) {
+      initOccupancy();
+    } else {
+      for (Map.Entry<V2i, Double> tileOccupancy : occupancy.entrySet()) {
+        V2i tile = tileOccupancy.getKey();
+        Double occupancyValue = tileOccupancy.getValue();
+        if (!occupancyValue.equals(0.0)) {
+          modifyTileOccupancy(tile, distributeWiped);
+        }
       }
     }
   }
